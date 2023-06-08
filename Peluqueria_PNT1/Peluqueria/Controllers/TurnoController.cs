e3ffb223bff5fcdf1757bf79f7100a94d4f39999
Emiliano Salcedo - ZENI <esalcedo@zeni.com.ar>
2023-06-07 22:26:51 -0300

    porfis aprobame

---
+++ Peluqueria_PNT1/Peluqueria/Controllers/TurnoController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Peluqueria.Context;
+using Peluqueria.Models;
+
+namespace Peluqueria.Controllers
+{
+    public class TurnoController : Controller
+    {
+        private readonly PeluqueriaDatabaseContext _context;
+
+        public TurnoController(PeluqueriaDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Turno
+        public async Task<IActionResult> Index()
+        {
+            var peluqueriaDatabaseContext = _context.Turno.Include(t => t.Cliente).Include(t => t.Servicio);
+            return View(await peluqueriaDatabaseContext.ToListAsync());
+        }
+
+        // GET: Turno/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno
+                .Include(t => t.Cliente)
+                .Include(t => t.Servicio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // GET: Turno/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id");
+            ViewData["ServicioId"] = new SelectList(_context.Servicio, "Id", "Id");
+            return View();
+        }
+
+        // POST: Turno/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FechaHora,Atendido,ServicioId,ClienteId")] Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", turno.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicio, "Id", "Id", turno.ServicioId);
+            return View(turno);
+        }
+
+        // GET: Turno/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno.FindAsync(id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", turno.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicio, "Id", "Id", turno.ServicioId);
+            return View(turno);
+        }
+
+        // POST: Turno/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FechaHora,Atendido,ServicioId,ClienteId")] Turno turno)
+        {
+            if (id != turno.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurnoExists(turno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Usuarios, "Id", "Id", turno.ClienteId);
+            ViewData["ServicioId"] = new SelectList(_context.Servicio, "Id", "Id", turno.ServicioId);
+            return View(turno);
+        }
+
+        // GET: Turno/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turno
+                .Include(t => t.Cliente)
+                .Include(t => t.Servicio)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // POST: Turno/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var turno = await _context.Turno.FindAsync(id);
+            _context.Turno.Remove(turno);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurnoExists(int id)
+        {
+            return _context.Turno.Any(e => e.Id == id);
+        }
+    }
+}
