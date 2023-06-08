4120c3b76b54bb3375f092eeee9430ffe42756a9
Emiliano Salcedo - ZENI <esalcedo@zeni.com.ar>
2023-06-07 22:05:31 -0300

    Arreglando lo de Abril...

--- Peluqueria_PNT1/Peluqueria/Migrations/20230607231203_Peluqueria.Context.PeluqueriaDatabaseContext.cs
+++ Peluqueria_PNT1/Peluqueria/Migrations/20230607231203_Peluqueria.Context.PeluqueriaDatabaseContext.cs
@@ -7,25 +7,25 @@ namespace Peluqueria.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-              name: "Usuarios",
-              columns: table => new
-              {
-                  Id = table.Column<int>(nullable: false)
-                      .Annotation("SqlServer:Identity", "1, 1"),
-                  Nombre = table.Column<string>(nullable: true),
-                  Apellido = table.Column<string>(nullable: true),
-                  Email = table.Column<string>(nullable: true),
-                  DNI = table.Column<string>(nullable: true),
-                  Direccion = table.Column<string>(nullable: true),
-                  Telefono = table.Column<string>(nullable: true),
-                  Contrasenia = table.Column<string>(nullable: true),
-                  Rol = table.Column<int>(nullable: false)
-              },
-              constraints: table =>
-              {
-                  table.PrimaryKey("PK_Usuarios", x => x.Id);
-              });
+            //migrationBuilder.CreateTable(
+            //  name: "Usuarios",
+            //  columns: table => new
+            //  {
+            //      Id = table.Column<int>(nullable: false)
+            //          .Annotation("SqlServer:Identity", "1, 1"),
+            //      Nombre = table.Column<string>(nullable: true),
+            //      Apellido = table.Column<string>(nullable: true),
+            //      Email = table.Column<string>(nullable: true),
+            //      DNI = table.Column<string>(nullable: true),
+            //      Direccion = table.Column<string>(nullable: true),
+            //      Telefono = table.Column<string>(nullable: true),
+            //      Contrasenia = table.Column<string>(nullable: true),
+            //      Rol = table.Column<int>(nullable: false)
+            //  },
+            //  constraints: table =>
+            //  {
+            //      table.PrimaryKey("PK_Usuarios", x => x.Id);
+            //  });
             migrationBuilder.CreateTable(
                 name: "Servicio",
                 columns: table => new
